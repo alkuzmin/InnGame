d42a3b45cfad3e76fb7a0b010745c1185fcac01a
kuzmin <kuz_alex_konst@mail.ru>
2024-03-02 13:14:18 +0400

    norm encapsulation of hashmap of resources

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.util.HashMap;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
@@ -8,5 +10,9 @@ public class Main {
         Unit u1 = new Unit("ork1");
         u1.setHealth(90);
         u1.doVoice();
+        //u1.getResources().put(ResTypes.FOOD, -100); !!!
+        u1.setResources(ResTypes.GOLD,20);
+        u1.setResources(ResTypes.ENERGY,67);
+        u1.printResources();
         }
     }
--- src/Unit.java
+++ src/Unit.java
@@ -1,4 +1,7 @@
+import java.sql.SQLOutput;
 import java.util.HashMap;
+
+enum ResTypes {GOLD, WATER, FOOD, ENERGY}
 
 public class Unit {
 
@@ -6,6 +9,7 @@ public class Unit {
     public Unit(String name)
     {
         this.setName(name);
+        this.resources = new HashMap<>();
 
     }
     private String name;
@@ -36,13 +40,19 @@ public class Unit {
         System.out.println("Iam "+this.name);
     }
 
-    private HashMap<String, Integer> resources;
+    private HashMap<ResTypes, Integer> resources;
 
-    public HashMap<String, Integer> getResources() {
-        return resources;
+    public HashMap<ResTypes, Integer> getResources() {
+        return new HashMap<ResTypes, Integer>(this.resources);
     }
 
-    public void setResources(HashMap<String, Integer> resources) {
-        this.resources = resources;
+    public void setResources(ResTypes restype, Integer val) {
+        if (val<0) throw new IllegalArgumentException();
+        this.resources.put(restype, val);
+    }
+
+    public void printResources()
+    {
+        this.resources.values().stream().forEach(System.out::println);
     }
 }
